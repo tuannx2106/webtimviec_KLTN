07afa2f83d228ff0fa8e905b72bd2f40f645544d
Son-97 <sonlqdev97@gmail.com>
2019-07-03 22:28:14 +0700

    merge

--- frontend_client/src/page/PageAllJob/component/Filter.js
+++ frontend_client/src/page/PageAllJob/component/Filter.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 
 class Filter extends Component {
-  constructor(props) {
+  constructor(props){
     super(props);
   }
 
@@ -13,13 +13,22 @@ class Filter extends Component {
         <button className="btn btn-primary col-lg-12 mb-3" onClick={onClickClearAll}>Tất cả công việc</button>
         <div>
           <div className="mb-3 p-2 bord bg-w">
-            <h6 className=" font mb-3 ">ĐỊA ĐIỂM</h6>
-            <div style={{ overflow: "auto", maxHeight: "220px" }}>
-              {cities && cities.map(item => (
-                <div className="select-wrap">
-                  <button className="form-control item-fil text-left" data-id={item.id} onClick={onClickCity}> {item.name} </button>
-                </div>
-              ))}
+            <h6 className="font mb-3 text-center" style={{ borderBottom: "1px solid #c1c1c1", paddingBottom: "5px" }}>Địa điểm</h6>
+            <div style={{ overflow: "auto", display: "block", maxHeight: "220px" }}>
+              <ul className="mb-0" style={{ lineHeight: "20px", listStyle: "none" }}>
+                {cities && cities.map(item => (
+                  <li className="super-search-filter">
+                    <label className="lable-filter">
+                      <span className="value item-fil" data-id={item.id} onClick={onClickCity}>{item.name}</span>
+                    </label>
+                  </li>
+                ))}
+                {/* {cities && cities.map(item => (
+                  <div className="select-wrap">
+                    <button className="form-control item-fil text-left" data-id={item.id} onClick={onClickCity}> {item.name} </button>
+                  </div>
+                ))} */}
+              </ul>
             </div>
           </div>
         </div>
